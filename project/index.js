7a655cbef71b37cf3e56a7a60b56a5f1e9c105b3
pinkal1 <pinkal.patel1991@gmail.com>
2017-06-25 14:34:36 -0700

    intial checkin
    Intial checkin+ Login logout comleted

---
+++ project/index.js
@@ -0,0 +1,14 @@
+
+
+/* 
+ * Student Info: Name=Parin patel tudent id:15154  * Subject: Cs526(a)_Homework3_2016  * Author: parin patel
+ 
+ * Filename: index.js
+ * Date and Time: Jul 30, 2016 5:32:17 AM
+ * Project Name: MakeADish
+ 
+ */
+
+
+
+
---
+++ project/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Smart Refrigerator</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <style>
+        html {
+  background-image: url('best3.jpg');
+  background-size: 1370px;
+
+}
+    </style>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Smart Refrigerator</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li ><a href="register.php">Registration</a></li>
+      <li><a href="featured.html">Featured Recipes</a>
+      </li>
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Inventory <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="listnotes.html">Manual added  Inventory</a></li>
+          <li><a href="addnotes1.html">Add Inventory</a></li>
+          <li><a href="add.html">Scanned Inventory</a></li>
+        </ul>
+      </li>
+        <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Type of Cuisine<span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="Indian.html">Indian</a></li>
+          <li><a href="Mexican.html">Mexican</a></li>
+          <li><a href="Italian.html">Italian</a></li>
+           <li><a href="health.html">Healthy Diet</a></li>
+        </ul>
+      </li>
+      
+      <li><a href="calendar.html">My Calendar</a></li>
+      <li><a href="Automatic.html">Automatic Ordering</a></li>
+      <li><a href="chat.html">Chat & Share</a></li>
+       <li><a href="health.html">Calorie & Step Count</a></li>
+      
+    </ul>
+   
+    <ul class="nav navbar-nav navbar-right">
+        <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="logout.php"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
+      </ul>
+  </div>
+</nav>
+    
+
+
+<div class="navbar navbar-default navbar-fixed-bottom">
+    <div class="container">
+        <p class="navbar-text">CopyRight Pinkal Patel</p>
+         <a href="https://twitter.com/"class="navbar-btn btn-success btn pull-right">Follow us on Twitter</a>
+        <a href="https://www.facebook.com/"class="navbar-btn btn-danger btn pull-right">Like us on Facebook</a>
+    </div>
+</div>
+
+
+
